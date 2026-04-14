67463c124745ccbf26c8e6815272938f6c60e836
eivmosn <eivmosn@163.com>
2026-04-14 11:40:58 +0800

    chore: css receipt border

---
+++ src/components/receipt/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-full bg-black p-4">
+    <div class="h-450px w-240px bg-#fff receipt px-10px pt-10px pb-20px">
+      <div class="h-full" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.receipt {
+  mask: conic-gradient(from -45deg at bottom,#0000,#000 1deg 89deg,#0000 90deg) 50%/9px 100%
+}
+</style>
